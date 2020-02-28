cfddec61995e252904f3fa22cb2eaddc0151cbba
zayneio <hello@zayne.io>
2020-02-28 11:54:20 -0500

    remove console logs

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,12 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
-  console.log('verifying...')
 
   const token = req.header('AUTH-TOKEN');
   if(!token) return res.status(401).send('Access Denied.');
-
-  console.log('token', token)
 
   try{
     const verified = jwt.verify(token, process.env.JWT_SECRET);
